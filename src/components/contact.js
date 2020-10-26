48d5402dbfab081c693ec936d8fb7f2a51233551
mssanoko <lawsonjleslie@gmail.com>
2020-10-26 08:27:26 -0400

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact Me</h1></div>
+            <div className="contact-body">
+            <Grid className="contact-grid">
+            <Cell col={6}>
+            <h2>Leslie Lawson</h2>
+            
+            <img 
+            src={"./avatar.png"}
+            alt="moi"
+            style={{height: '250px'}}
+            />
+            <p style={{width: '75px', margin: 'auto', paddingTop: '1em'}}>Web Developer Full Stack</p>
+            </Cell>
+
+            <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+            <List>
+            <ListItem>
+            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}> 
+            <i className="fa fa-envelope" aria-hidden="true"/> lawsonjleslie@gmail.com
+            </ListItemContent>
+            </ListItem>
+            <ListItem>
+            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+            <i className="fa fa-phone-square" aria-hidden="true"/> (862) 448-4900
+            </ListItemContent>
+            </ListItem>
+            <ListItem>
+            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    lilsly95
+            </ListItemContent>
+            </ListItem>
+            </List>
+            </div>
+            </Cell>
+            </Grid>
+            </div>
         )
     }
 }
--- src/components/porfolio.js
+++ src/components/porfolio.js
@@ -1,25 +1,193 @@
 import React, { Component } from 'react';
-import {Tabs, Tab } from 'react-mdl';
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton} from 'react-mdl';
+
 
 class Porfolio extends Component {
     constructor(props) {
         super (props);
         this.state = { activeTab: 0 };
     }
+    toggleCategories() {
 
+        if(this.state.activeTab === 0) {
+            return(
+                <div ClassName="porfolio-grid">
+                {/*Project 1*/}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>What am I eating?</CardTitle>
+                    <CardText>
+                    Utilizing this web application will provide users a randomized meal and cocktail after using androp down selection menu to find a meal closer to their preference.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+
+                {/*Project 2*/}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Binvoice</CardTitle>
+                    <CardText>
+                   As a contractor I would like an affordable app to help me generate invoices for my customers.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+
+            )
+        } else if(this.state.activeTab === 1) {
+            return(
+                <div ClassName="porfolio-grid">
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Weather Dashboard</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Day Planner</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Code Quiz</CardTitle>
+                <CardActions border>
+                    <Button colored>Link</Button>
+                    <Button colored>Github</Button>
+                </CardActions>
+                <CardMenu style={{color:'#d8bfd8'}}>
+                    <IconButton name="share" />
+                </CardMenu>
+            </Card>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Password Generator</CardTitle>
+            <CardActions border>
+                <Button colored>Link</Button>
+                <Button colored>Github</Button>
+            </CardActions>
+            <CardMenu style={{color:'#d8bfd8'}}>
+                <IconButton name="share" />
+            </CardMenu>
+        </Card>
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Good ReadMe Generator</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Team Profile Generator</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Note Taker</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Employer Tracker</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Burger App</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Fitness Tracker</CardTitle>
+                <CardActions border>
+                    <Button colored>Link</Button>
+                    <Button colored>Github</Button>
+                </CardActions>
+                <CardMenu style={{color:'#d8bfd8'}}>
+                    <IconButton name="share" />
+                </CardMenu>
+            </Card>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>Progressive Budget</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: "#d8bfd8", height: '176px', background: 'url() center / cover'}}>User Directory</CardTitle>
+                    <CardActions border>
+                        <Button colored>Link</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#d8bfd8'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        }
+}
+
+    
     render() {
         return(
-            <div className= "category-tabs">
+            <div>
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
-                <Tab>REACT</Tab>
-                <Tab>NODEJS</Tab>
-                <Tab>MYSQL</Tab>
-                <Tab>MONGODB</Tab>
+                <Tab>PROJECTS</Tab>
+                <Tab>ASSIGNMENTS</Tab>
                 </Tabs>
 
-                <section className="porfolio-grid">
-                    {this.toggleCategories()}
-                </section>
+                
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+                   
             </div>
         )
     }
