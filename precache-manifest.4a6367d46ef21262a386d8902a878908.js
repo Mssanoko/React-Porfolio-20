7d999f32489b8b7f4d5e29228be443225ae3ff09
mssanoko <lawsonjleslie@gmail.com>
2020-10-27 00:48:51 -0400

    Updates

---
+++ precache-manifest.4a6367d46ef21262a386d8902a878908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b395eae92583ca480b93892835514e7c",
+    "url": "/React-Porfolio-20/index.html"
+  },
+  {
+    "revision": "d0c9178431b4e9e13026",
+    "url": "/React-Porfolio-20/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "c92df1a2c63cee106dac",
+    "url": "/React-Porfolio-20/static/css/main.0f0cfba2.chunk.css"
+  },
+  {
+    "revision": "d0c9178431b4e9e13026",
+    "url": "/React-Porfolio-20/static/js/2.fc05d8a0.chunk.js"
+  },
+  {
+    "revision": "12393ff7ff42d28b511204d747ac4732",
+    "url": "/React-Porfolio-20/static/js/2.fc05d8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92df1a2c63cee106dac",
+    "url": "/React-Porfolio-20/static/js/main.189b491a.chunk.js"
+  },
+  {
+    "revision": "eee5e0f67e9f788430f3",
+    "url": "/React-Porfolio-20/static/js/runtime-main.eff4ed7d.js"
+  }
+]);
